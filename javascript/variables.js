8618094026d9a13ae918846131d46254a3f4cca3
fathisahar <sahar.fathi@emailgmail.info>
2023-07-25 10:12:41 -0400

    variables

---
+++ javascript/variables.js
@@ -0,0 +1,10 @@
+/*
+let is the preferred way to declare a variable when it can be reassigned
+const is the preferred way to declare a variable with a constant value.
+
+variables that have not been initialized store the primitive data type undefined.
+*/
+
+let interpolation = 20;
+myName = 'SAHAR'.toLowerCase();
+console.log(`Interpolation uses template literals to place data while logging: My name is ${myName} and I am ${interpolation} years old.`);
