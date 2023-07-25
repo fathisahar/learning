4ea61d59e8500d748ffe678a4b61a59202814043
fathisahar <sahar.fathi@emailgmail.info>
2023-07-25 11:12:30 -0400

    conditionals

--- javascript/conditionals.js
+++ javascript/conditionals.js
@@ -19,7 +19,6 @@ comparison operators:
  }
 
  /*
-
  falsy:
     false, 0, '', null, undefined, NaN
 */
@@ -35,21 +34,35 @@ console.log(`The ${writingUtensil} is mightier than the sword.`);
 
 /* 
 ternary operator: 
-The condition is provided before the ?.
-Two expressions follow the ? and are separated by a colon :.
-If the condition evaluates to true, the first expression executes.
-If the condition evaluates to false, the second expression executes.
+    condition before the ?
+    Two expressions after ? separated by a colon :
+    If the condition evaluates to true, the first expression executes
+    If the condition evaluates to false, the second expression executes
 */
 
 let isLocked = false;
-
 isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
 
-
 let isCorrect = true;
-
 isCorrect ? console.log('Correct!') : console.log('Incorrect!');
 
 let favoritePhrase = 'Love That!';
-
 favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+//switch statement
+
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+}
