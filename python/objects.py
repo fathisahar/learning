429134ec24803513d3d559592eb66e46e8ca0790
fathisahar <sahar.fathi@emailgmail.info>
2023-07-27 14:11:40 -0400

    overriding and inheritance

--- python/objects.py
+++ python/objects.py
@@ -32,3 +32,47 @@ e3 = Admin()
 e1.say_id()
 e2.say_id()
 e3.say_id()
+
+#EXAMPLE SUPER
+class Animal:
+  def __init__(self, name, sound="Grrrr"):
+    self.name = name
+    self.sound = sound
+ 
+  def make_noise(self):
+    print("{} says, {}".format(self.name, self.sound))
+ 
+class Cat(Animal):
+  def __init__(self, name):
+    super().__init__(name, "Meow!") 
+ 
+pet_cat = Cat("Rachel")
+pet_cat.make_noise() # Rachel says, Meow!
+
+
+#MULTIPLE INHERITANCE
+class Employee():
+  new_id = 1
+  def __init__(self):
+    self.id = Employee.new_id
+    Employee.new_id += 1
+
+  def say_id(self):
+    print("My id is {}.".format(self.id))
+
+class Admin(Employee):
+  def say_id(self):
+    super().say_id()
+    print("I am an admin.")
+
+# Write your code below
+class Manager(Admin):
+    def say_id(self):
+      print("I am in charge")
+      super().say_id()
+
+e1 = Employee()
+e2 = Employee()
+e3 = Admin()
+e4 = Manager()
+e4.say_id()
